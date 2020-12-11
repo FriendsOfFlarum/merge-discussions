79ae9d9e66e089566bd69c19e167e0c2642b671f
karaok <karaok491@gmail.com>
2020-12-11 23:00:58 +0000

    beta 15 prep

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,16 +1,12 @@
-import PermissionGrid from 'flarum/components/PermissionGrid';
-import { extend } from 'flarum/extend';
+import app from 'flarum/app';
 
 app.initializers.add('fof/merge-discussions', () => {
-    extend(PermissionGrid.prototype, 'moderateItems', items => {
-        items.add(
-            'mergeDiscussions',
-            {
-                icon: 'fas fa-code-branch fa-flip-vertical',
-                label: app.translator.trans('fof-merge-discussions.admin.permissions.merge_discussions_label'),
-                permission: 'discussion.merge',
-            },
-            64
-        );
-    });
+    app.extensionData.for('fof-merge-discussions').registerPermission(
+        {
+            icon: 'fas fa-code-branch fa-flip-vertical',
+            label: app.translator.trans('fof-merge-discussions.admin.permissions.merge_discussions_label'),
+            permission: 'discussion.merge',
+        },
+        'moderate'
+    );
 });
