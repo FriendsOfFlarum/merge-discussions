56e729b71034885c11dd7fa77cb9616c9478af76
IanM <ian@morland.me>
2025-11-29 10:26:24 +0000

    fix: typing

--- js/src/forum/components/DiscussionMergedNotification.ts
+++ js/src/forum/components/DiscussionMergedNotification.ts
@@ -1,5 +1,6 @@
 import app from 'flarum/forum/app';
 import Notification from 'flarum/forum/components/Notification';
+import type Discussion from 'flarum/common/models/Discussion';
 
 export default class DiscussionMergedNotification extends Notification {
   icon() {
@@ -8,7 +9,11 @@ export default class DiscussionMergedNotification extends Notification {
 
   href() {
     const notification = this.attrs.notification;
-    const discussion = notification.subject();
+    const discussion = notification.subject() as Discussion | null;
+
+    if (!discussion) {
+      return '#';
+    }
 
     return app.route.discussion(discussion);
   }
@@ -16,8 +21,8 @@ export default class DiscussionMergedNotification extends Notification {
   content() {
     const notification = this.attrs.notification;
     const user = notification.fromUser();
-    const oldDiscussion = notification.content();
-    const oldTitle = oldDiscussion.merged_title;
+    const oldDiscussion = notification.content() as { merged_title?: string } | undefined;
+    const oldTitle = oldDiscussion?.merged_title || '';
 
     return app.translator.trans('fof-merge-discussions.forum.notification.discussion_merged', {
       user,
