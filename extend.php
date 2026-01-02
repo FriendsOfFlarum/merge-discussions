b5d65b27b9e844e44f6851ba3fb71119c1a642bf
IanM <ian@morland.me>
2026-01-02 17:28:37 +0000

    chore: use code splitting to reduce js bundle size

--- extend.php
+++ extend.php
@@ -21,6 +21,7 @@ use FoF\MergeDiscussions\Posts\DiscussionMergePost;
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
+        ->jsDirectory(__DIR__.'/js/dist/forum')
         ->css(__DIR__.'/resources/less/forum.less'),
 
     (new Extend\Frontend('admin'))
--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,10 +1,10 @@
 import app from 'flarum/forum/app';
-import addNotifications from './addNotifications';
 import extendDiscussionControls from './extendDiscussionControls';
+import extendNotifications from './extendNotifications';
 
 export { default as extend } from './extend';
 
 app.initializers.add('fof/merge-discussions', () => {
   extendDiscussionControls();
-  addNotifications();
+  extendNotifications();
 });
